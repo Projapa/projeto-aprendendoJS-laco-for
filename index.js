648f5d16dd3c35cfac4f0b33af7e4baab11a79cd
Danillo Araki <121911759+Projapa@users.noreply.github.com>
2023-07-24 11:13:40 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// for (inicializador; condicao; incremento){
+//delcaracao 
+//}
+
+// for(let i = 0; i < 5; i++){
+//     console.log('Olá Mundo!')
+// }
+
+console.log('Olá Mundo!');
+console.log('Olá Mundo!');
+console.log('Olá Mundo!');
+console.log('Olá Mundo!');
+console.log('Olá Mundo!');
